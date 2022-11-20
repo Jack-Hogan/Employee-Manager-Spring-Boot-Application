900ab70737736bd274109e0bfbde908b1d346cc1
Jack-Hogan <jack.hogan.06@hotmail.com>
2022-11-20 12:16:44 +0000

    Complete Employee Manager Backend

---
+++ src/main/java/com/springlearning/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package com.springlearning.employeemanager.repo;
+
+import com.springlearning.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
